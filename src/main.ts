161a338d07750f68f81ee4cb2c2c615472b7cb77
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-04-08 10:56:46 -0300

    chore: remove swagger for now

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import fastify from "fastify";
-
-// import routes from "./interface/routes";
+import jwt from "@fastify/jwt";
+import cors from "@fastify/cors";
 
 const server = fastify({
   logger: true,
+});
+
+server.register(jwt, {
+  secret: "temp",
+});
+
+server.register(cors, {
+  origin: "*",
 });
 
 server.listen({ port: 3333 }, function (err) {
